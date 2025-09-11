6c61711fc8bbae6c491a52c0f59e5599d6cdef90
Peinoi <124995489+Peinoi@users.noreply.github.com>
2025-09-11 17:23:01 +0900

    Update condition.js
    스위치문 if문 버전 추가

--- public/js/condition.js
+++ public/js/condition.js
@@ -43,3 +43,24 @@ function calculate(){
   console.log(first+" "+last+" "+par);  
 
 }//end of calculate().
+
+function calculateif(){
+  let first =parseInt(document.querySelector('#user1').value);
+  let last = parseInt(document.querySelector('#user2').value);
+  
+  let par = document.querySelector("#oper").value;
+  let result;
+  if(par=="+"){
+    result = first+last;
+  }
+  else if(par=="-"){
+  result = first-last;
+  } else if(par=="*"){
+  result = first*last;
+  } else if(par=="/"){
+  result = first/last;
+  }
+  document.querySelector('#result').value=result;
+  console.log(first+" "+last+" "+par);  
+
+}//end of calculate().
