1a31a28aa0c883828f74985bb239195ba26dabd1
master <mymail@google.com>
2025-09-11 16:40:00 +0900

    if조건문 완료

--- public/js/condition.js
+++ public/js/condition.js
@@ -1,1 +1,18 @@
-//안녕하세요
+function checkScore() {
+  const score = parseInt(document.querySelector("#user_value").value)
+  if (score >= 90) {
+    console.log("참잘함");
+  }
+  if (score >= 80) {
+    console.log("잘함");
+  }
+  if (score >= 70) {
+    console.log("보통");
+  }
+  if (score >= 60) {
+    console.log("노력");
+  } else {
+    console.log("낙제");
+
+  }
+}
